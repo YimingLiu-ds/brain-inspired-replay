618e5bac2b1058ac3d58eb1b978261ee86f977c0
jackmillichamp <53472220+jackmillichamp@users.noreply.github.com>
2021-06-24 17:53:40 +0100

    Update rep_hypers.py

--- rep_hypers.py
+++ rep_hypers.py
@@ -27,7 +27,7 @@ result = subprocess.run(['tsp', '-S 1', 'hare', 'run', '--rm', '--workdir /app',
          '--user $(id', '-u):$(id', '-g)', '--gpus', '\'\"device=3,4\"\'', 'jlm67/project', \
          'python3', 'main_cl.py', '--experiment=splitMNIST', '--scenario=class', \
          '--replay=generative', '--brain-inspired', '--si', '--repulsion', '--kl-js={}'.format('js'), \
-         '--use-rep-f', '--rep-f={}'.format(1.5), '--tuning', 'iters=50'], capture_output=True, text=True)
-
+         '--use-rep-f', '--rep-f={}'.format(1.5), '--tuning', 'iters=50'])
+result.check_returncode()
 print("stdout:", result.stdout)
 print("stderr:", result.stderr)   

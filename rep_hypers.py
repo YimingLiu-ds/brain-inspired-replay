df81d0a14178f49891ca6d96715dfbd529c2c8ca
jackmillichamp <53472220+jackmillichamp@users.noreply.github.com>
2021-06-24 17:14:27 +0100

    Update rep_hypers.py

--- rep_hypers.py
+++ rep_hypers.py
@@ -17,7 +17,8 @@ f_list = [1.25, 1.5, 2, 2.5, 3, 3.5, 4, 5]
 
 for kl_js in kl_js_list:
     for f in f_list:
-        subprocess.run(['tsp', '-S 8', \
+        subprocess.run(['tsp', '-S 8', 'hare', 'run', '--rm', '--workdir /app', '-v "$(pwd)":/app', \
+         '--user $(id', '-u):$(id', '-g)', '--gpus', '\'\"device=3,4\"\'', 'jlm67/project', \
          'python', 'main_cl.py', '--experiment=splitMNIST', '--scenario=class', \
          '--replay=generative', '--brain-inspired', '--si', '--repulsion', '--kl-js={}'.format(kl_js), \
          '--use-rep-f', '--rep-f={}'.format(f), 'iters=10'])

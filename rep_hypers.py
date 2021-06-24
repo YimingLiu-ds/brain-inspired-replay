963784651b2e86a8fe404c0a850cae92f3a4a394
jackmillichamp <53472220+jackmillichamp@users.noreply.github.com>
2021-06-24 17:11:54 +0100

    Update rep_hypers.py

--- rep_hypers.py
+++ rep_hypers.py
@@ -8,7 +8,6 @@ Created on Thu Jun 24 16:25:19 2021
 #! /usr/bin/env python
 
 import subprocess
-import numpy
 
 # Whether to use KL or JS divergence...
 kl_js_list = ['kl', 'js']
@@ -18,9 +17,13 @@ f_list = [1.25, 1.5, 2, 2.5, 3, 3.5, 4, 5]
 
 for kl_js in kl_js_list:
     for f in f_list:
-        subprocess.run(['tsp', '-S 8', 'hare', 'run', '--rm', '--workdir /app', '-v "$(pwd)":/app', \
-         '--user $(id', '-u):$(id', '-g)', '--gpus', '\'\"device=3,4\"\'', 'jlm67/project', \
+        subprocess.run(['tsp', '-S 8', \
          'python', 'main_cl.py', '--experiment=splitMNIST', '--scenario=class', \
          '--replay=generative', '--brain-inspired', '--si', '--repulsion', '--kl-js={}'.format(kl_js), \
          '--use-rep-f', '--rep-f={}'.format(f), 'iters=10'])
-        
+
+#['tsp', '-S 8', 'hare', 'run', '--rm', '--workdir /app', '-v "$(pwd)":/app', \
+#         '--user $(id', '-u):$(id', '-g)', '--gpus', '\'\"device=3,4\"\'', 'jlm67/project', \
+#         'python', 'main_cl.py', '--experiment=splitMNIST', '--scenario=class', \
+#         '--replay=generative', '--brain-inspired', '--si', '--repulsion', '--kl-js={}'.format(kl_js), \
+#         '--use-rep-f', '--rep-f={}'.format(f), 'iters=10']      
